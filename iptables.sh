96da7a8fcca3343d43b2f90fc1bc64ad6d1f13b2
荒海 輝夫 <araumi@hand.co.jp>
2014-04-25 19:56:24 +0900

    アウトプットの許可の仕方をスタイリッシュにした。establishedを使うようにしただけですすいません

--- iptables.sh
+++ iptables.sh
@@ -12,7 +12,9 @@ LOCAL_IP=192.168.56.200
 
 # 初期化の後に呼ぶように
 iptables -A INPUT -p tcp -s $ALLOW_IP -d $LOCAL_IP --dport 22 -j ACCEPT
-iptables -A OUTPUT -p tcp -s $LOCAL_IP --sport 22 -d $ALLOW_IP -j ACCEPT
+
+# 確立している通信はアウトプットを許可
+iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 iptables -N LOGGING
 iptables -A LOGGING -j LOG --log-level warning --log-prefix "DROP:" -m limit
