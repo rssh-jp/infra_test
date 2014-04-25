71d296ef2c879a10254ee75add6797a7f842d50e
荒海 輝夫 <araumi@hand.co.jp>
2014-04-25 16:22:13 +0900

    iptablesのシェルファイル登録

---
+++ iptables.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+iptables -F
+
+# ALLOW_IP=10.1.1.54
+ALLOW_IP=192.168.56.1
+LOCAL_IP=192.168.56.200
+
+iptables -A INPUT -p tcp -s $ALLOW_IP -d $LOCAL_IP --dport 22 -j ACCEPT
+iptables -A OUTPUT -p tcp -s $LOCAL_IP --sport 22 -d $ALLOW_IP -j ACCEPT
+
+iptables -P INPUT DROP
+iptables -P OUTPUT DROP
+iptables -P FORWARD DROP
+
+iptables -N LOGGING
+iptables -A LOGGING -j LOG --log-level warning --log-prefix "DROP:" -m limit
+iptables -A LOGGING -j DROP
+iptables -A INPUT -j LOGGING
+
